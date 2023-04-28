383f5a369edfa9adf5fbe8129f5bb98086914376
Harsh <kumarharsh74799gmail.com>
2023-04-28 15:18:18 +0530

    This is my first repo

---
+++ app/src/main/java/com/google/firebase/udacity/notificationapp/myApplication.java
@@ -0,0 +1,25 @@
+package com.google.firebase.udacity.notificationapp;
+
+import android.app.Application;
+
+import com.onesignal.OneSignal;
+
+public class myApplication  extends Application {
+    private static final String ONESIGNAL_APP_ID = "69409edb-1472-4eb0-9287-d160495fc258";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+
+        // promptForPushNotifications will show the native Android notification permission prompt.
+        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 7)
+        OneSignal.promptForPushNotifications();
+    }
+}
